f3eb377ca4e300a14946d737a13abda2f975fea8
Thomas Nilsson <tnni@dtu.dk>
2018-11-16 18:56:13 +0100

    Pedometer pubspec update

--- packages/pedometer/example/lib/main.dart
+++ packages/pedometer/example/lib/main.dart
@@ -20,21 +20,21 @@ class _MyAppState extends State<MyApp> {
 
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
+    setUpPedometer();
+    if (!mounted) return;
+  }
+
+  void setUpPedometer() {
     Pedometer pedometer = new Pedometer();
     _subscription = pedometer.stepCountStream.listen(_onData,
         onError: _onError, onDone: _onDone, cancelOnError: true);
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
   }
 
   void _onData(int stepCountValue) async {
     setState(() => _stepCountValue = "$stepCountValue");
   }
 
-  void _onDone() {}
+  void _onDone() => print("Finished pedometer tracking");
 
   void _onError(error) => print("Flutter Pedometer Error: $error");
 
