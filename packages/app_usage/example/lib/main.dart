399d6eaa38dd853679df7b889447b13f4d86c856
Thomas Nilsson <tnni@dtu.dk>
2018-11-19 14:13:54 +0100

    App usage: Example app up and running

--- packages/app_usage/example/lib/main.dart
+++ packages/app_usage/example/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
-
 import 'package:app_usage/app_usage.dart';
 
 void main() => runApp(MyApp());
@@ -11,7 +10,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  AppUsage usage = new AppUsage();
+  AppUsage appUsage = new AppUsage();
+  String apps = 'Unknown';
 
 
   @override
@@ -24,18 +24,39 @@ class _MyAppState extends State<MyApp> {
   Future<void> initPlatformState() async {
   }
 
-  void getUsageStats() {
+  void getUsageStats() async {
     DateTime endDate = new DateTime.now();
     DateTime startDate = DateTime(endDate.year, endDate.month, endDate.day, 0, 0, 0);
-    usage.getUsage(startDate, endDate);
+    Map<String, double> usage = await appUsage.getUsage(startDate, endDate);
+    usage.removeWhere((key,val) => val == 0);
+//    print(usage);
+    setState(() => apps = makeString(usage));
   }
+
+  String makeString(Map<String, double> usage) {
+    String result = '';
+    usage.forEach((k,v) {
+      String appName = k.split('.').last;
+      String timeInMins = (v / 60).toStringAsFixed(2);
+      result += '$appName : $timeInMins minutes\n';
+    });
+    return result;
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('App Usage Example'),
+        ),
+        body: Text(
+          apps,
+          overflow: TextOverflow.ellipsis,
+          style: TextStyle(
+            fontSize: 20.0, // insert your font size here
+          ),
         ),
         floatingActionButton: FloatingActionButton(
             onPressed: getUsageStats,
--- packages/app_usage/lib/app_usage.dart
+++ packages/app_usage/lib/app_usage.dart
@@ -6,13 +6,13 @@ class AppUsage {
   static const MethodChannel _methodChannel =
       const MethodChannel("app_usage.methodChannel");
 
-  void getUsage(DateTime startDate, DateTime endDate) async {
+  Future<Map> getUsage(DateTime startDate, DateTime endDate) async {
     int end = endDate.millisecondsSinceEpoch;
     int start = startDate.millisecondsSinceEpoch;
 
     Map<String, int> interval = {'start': start, 'end': end};
     Map<dynamic, dynamic> usage = await _methodChannel.invokeMethod('getUsage', interval);
 
-    print(usage);
+    return Map<String, double>.from(usage);
   }
 }
