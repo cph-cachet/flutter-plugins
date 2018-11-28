64e8e41f95d752be3656039706feb7b31ce8d98a
Thomas Nilsson <tnni@dtu.dk>
2018-11-28 23:06:44 +0100

    Weather object in the making

--- packages/weather/lib/weather.dart
+++ packages/weather/lib/weather.dart
@@ -3,6 +3,38 @@ import 'dart:convert';
 import 'package:location/location.dart';
 import 'package:http/http.dart' as http;
 
+class WeatherResult {
+  String _country, _placeName, _weatherMain, _weatherDescription;
+  DateTime _sunrise, _sunset;
+  double _latitude,
+      _longitude,
+      _temperature,
+      _humidity,
+      _pressure,
+      _tempMin,
+      _tempMax,
+      _windSpeed,
+      _windDegree;
+
+  WeatherResult(Map<String, dynamic> weatherData) {
+    _latitude = weatherData['coord']['lat'];
+    _longitude = weatherData['coord']['lon'];
+
+    _country = weatherData['country'];
+    _placeName = weatherData[''];
+    _weatherMain = weatherData[''];
+    _weatherDescription = weatherData[''];
+    _sunrise = weatherData['sunrise'];
+    _sunset = weatherData['sunset'];
+    _humidity = weatherData[''];
+    _pressure = weatherData[''];
+    _tempMin = weatherData[''];
+    _tempMax = weatherData[''];
+    _windSpeed = weatherData[''];
+    _windDegree = weatherData[''];
+  };
+}
+
 /// Plugin for fetching weather data in JSON.
 class Weather {
   String _apiKey;
@@ -40,8 +72,7 @@ class Weather {
     Map<String, double> loc = await new Location().getLocation();
     double lat = loc['latitude'];
     double lon = loc['longitude'];
-    return
-        'http://api.openweathermap.org/data/2.5/' +
+    return 'http://api.openweathermap.org/data/2.5/' +
         '$tag?lat=$lat&lon=$lon&appid=$_apiKey';
   }
 }
