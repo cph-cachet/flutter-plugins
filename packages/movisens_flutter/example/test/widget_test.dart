322e4c61b691d967a8e7a7899ebb22c74b004013
thomasnilsson <s144470@student.dtu.dk>
2019-01-28 14:38:28 +0100

    Movisens: Classes for user data, and removed logs

--- packages/movisens_flutter/example/test/widget_test.dart
+++ packages/movisens_flutter/example/test/widget_test.dart
@@ -4,19 +4,13 @@
 // find child widgets in the widget tree, read text, and verify that the values of widget properties
 // are correct.
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:movisens_flutter_example/main.dart';
-import 'dart:convert';
+import 'package:movisens_flutter/movisens_flutter.dart';
 
 void main() {
+  int weight = 100, height = 180, age = 25;
+  String address = '88:6B:0F:82:1D:33', name = 'Sensor 02655';
+  UserData userData = new UserData(
+      weight, height, Gender.male, age, SensorLocation.chest, address, name);
 
+  print(userData.asMap);
 }
-
-//void main() {
-//  test("Unit test", () {
-//    String json = '{moderate=0.0, light=0.0, sedentary=60.0, vigorous=0.0}';
-//    print(jsonDecode(json));
-//  });
-//}
--- packages/movisens_flutter/lib/movisens_flutter.dart
+++ packages/movisens_flutter/lib/movisens_flutter.dart
@@ -3,6 +3,52 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 import 'dart:convert';
 
+enum Gender { male, female }
+
+enum SensorLocation {
+  left_ankle,
+  left_hip,
+  left_thigh,
+  left_upper_arm,
+  left_wrist,
+  right_ankle,
+  right_hip,
+  right_thigh,
+  right_upper_arm,
+  right_wrist,
+  chest
+}
+
+class UserData {
+  int weight, height, age;
+
+  /// Weight in kg, height in cm, age in years
+  Gender gender;
+
+  /// Gender: male or female
+  SensorLocation sensorLocation;
+
+  /// Sensor placement on body
+  String sensorAddress, sensorName;
+
+  /// Sensor device addresss and name
+
+  UserData(this.weight, this.height, this.gender, this.age, this.sensorLocation,
+      this.sensorAddress, this.sensorName);
+
+  Map<String, String> get asMap {
+    return {
+      'weight': '$weight',
+      'height': '$height',
+      'age': '$age',
+      'gender': '$gender',
+      'sensor_location': '$sensorLocation',
+      'sensor_address': '$sensorAddress',
+      'sensor_name': '$sensorName'
+    };
+  }
+}
+
 /// Keys for Movisens data points
 const String TAP_MARKER = 'tap_marker',
     BATTERY_LEVEL = 'battery_level',
@@ -31,10 +77,8 @@ class MovisensMetLevel extends MovisensDataPoint {
   MovisensMetLevel(String metLevelString) {
     String metLevelJson = metLevelString.replaceAllMapped(
         new RegExp(r'([a-z]+)\=([\d.]+)'), (g) => '"${g[1]}":"${g[2]}"');
-    print(metLevelJson);
     Map<String, dynamic> metLevel = jsonDecode(metLevelJson);
 
-  // print("After Decode json  "+jsonDecode(metLevelJson));
     _sedentary = double.parse(metLevel['sedentary']);
     _light = double.parse(metLevel['light']);
     _moderate = double.parse(metLevel['moderate']);
@@ -48,6 +92,17 @@ class MovisensMetLevel extends MovisensDataPoint {
   double get moderate => _moderate;
 
   double get vigorous => _vigorous;
+
+  @override
+  String toString() {
+    return 'MetLevel: {'
+        'time: $timeStamp, '
+        'sedentary: $sedentary, '
+        'light: $light, '
+        'moderate: $moderate, '
+        'vigorous: $vigorous'
+        '}';
+  }
 }
 
 /// Battery level of the Movisens device, in percent (%)
@@ -59,6 +114,14 @@ class MovisensBatteryLevel extends MovisensDataPoint {
   }
 
   double get batteryLevel => _batteryLevel;
+
+  @override
+  String toString() {
+    return 'BatteryLevel: {'
+        'time: $timeStamp, '
+        'battery_level: $batteryLevel'
+        '}';
+  }
 }
 
 /// Step count monitored by the Movisens device
@@ -70,20 +133,43 @@ class MovisensStepCount extends MovisensDataPoint {
   }
 
   int get stepCount => _stepCount;
+
+  @override
+  String toString() {
+    return 'StepCount: {'
+        'time: $timeStamp, '
+        'step_count: $stepCount'
+        '}';
+  }
 }
 
 /// A generic class which only contains a timestamp, for when the movisens device was tapped.
-class MovisensTapMarker extends MovisensDataPoint {}
+class MovisensTapMarker extends MovisensDataPoint {
+  @override
+  String toString() {
+    return 'TapMarker: {'
+        'time: $timeStamp'
+        '}';
+  }
+}
 
 class MovisensMet extends MovisensDataPoint {
   double _met;
-   MovisensMet(dynamic value) {
+
+  MovisensMet(dynamic value) {
     String met = value;
-    print("customMet = " +value);
-    _met = double.parse( met.split(',').removeLast());
+    _met = double.parse(met.split(',').removeLast());
   }
 
   double get met => _met;
+
+  @override
+  String toString() {
+    return 'MET: {'
+        'time: $timeStamp, '
+        'met: $met'
+        '}';
+  }
 }
 
 /// Movisens body-position, which depends on the sensor location
@@ -93,6 +179,14 @@ class MovisensBodyPosition extends MovisensDataPoint {
   MovisensBodyPosition(this._bodyPosition);
 
   String get bodyPosition => _bodyPosition;
+
+  @override
+  String toString() {
+    return 'BodyPosition: {'
+        'time: $timeStamp, '
+        'body_position: $bodyPosition'
+        '}';
+  }
 }
 
 /// Accelerometer measure of the Movisens device
@@ -104,13 +198,19 @@ class MovisensMovementAcceleration extends MovisensDataPoint {
   }
 
   double get met => _movementAcceleration;
+
+  @override
+  String toString() {
+    return 'MovementAcceleration: {'
+        'time: $timeStamp, '
+        'movement_acceleration: $_movementAcceleration'
+        '}';
+  }
 }
 
 /// Factory function for converting a generic object sent through the platform channel into a concrete [MovisensDataPoint] object.
 MovisensDataPoint movisensFactory(dynamic javaMap) {
   Map<String, dynamic> data = Map<String, dynamic>.from(javaMap);
-  print(data);
-
   String _batteryLevel =
       data.containsKey(BATTERY_LEVEL) ? data[BATTERY_LEVEL] : null;
   String _tapMarker = data.containsKey(TAP_MARKER) ? data[TAP_MARKER] : null;
@@ -149,8 +249,8 @@ class MovisensFlutter {
 
   /// Sends data used for starting the Movisens device.
   /// The sampling will begin once the data is received on the other end of the [MethodChannel].
-  void startSensing(Map<String, String> userData) {
-    Map<String, dynamic> args = {'user_data': userData};
+  void startSensing(UserData userData) {
+    Map<String, dynamic> args = {'user_data': userData.asMap};
     _methodChannel.invokeMethod('userData', args);
   }
 }
