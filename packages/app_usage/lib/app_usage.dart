f2052acab4b2fdfd1e1dd3d973340f91cee6fe5f
thomasnilsson <s144470@student.dtu.dk>
2019-02-13 18:07:55 +0100

    App usage ios fix

--- packages/app_usage/lib/app_usage.dart
+++ packages/app_usage/lib/app_usage.dart
@@ -1,17 +1,23 @@
 import 'dart:async';
 
 import 'package:flutter/services.dart';
+import 'dart:io' show Platform;
 
 class AppUsage {
   static const MethodChannel _methodChannel =
       const MethodChannel("app_usage.methodChannel");
 
   Future<Map> getUsage(DateTime startDate, DateTime endDate) async {
-    int end = endDate.millisecondsSinceEpoch;
-    int start = startDate.millisecondsSinceEpoch;
+    Map<dynamic, dynamic> usage = new Map();
+    if (Platform.isAndroid) {
+      int end = endDate.millisecondsSinceEpoch;
+      int start = startDate.millisecondsSinceEpoch;
 
-    Map<String, int> interval = {'start': start, 'end': end};
-    Map<dynamic, dynamic> usage = await _methodChannel.invokeMethod('getUsage', interval);
+      Map<String, int> interval = {'start': start, 'end': end};
+      usage = await _methodChannel.invokeMethod('getUsage', interval);
+    } else if (Platform.isIOS) {
+      usage['error.n/a'] = 0.0;
+    }
 
     return Map<String, double>.from(usage);
   }
