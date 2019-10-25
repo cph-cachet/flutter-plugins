6fd74a61a99d542bfc2b16d6a30fd05150f4163f
thomasnilsson <s144470@student.dtu.dk>
2019-10-25 15:25:32 +0200

    cleaned up swift code a lot nowø

--- packages/flutter_health/ios/Classes/SwiftFlutterHealthPlugin.swift
+++ packages/flutter_health/ios/Classes/SwiftFlutterHealthPlugin.swift
@@ -6,6 +6,8 @@ public class SwiftFlutterHealthPlugin: NSObject, FlutterPlugin {
     
     let healthStore = HKHealthStore()
     var healthDataTypes = [HKSampleType]()
+    var heartRateEventTypes = Set<HKSampleType>()
+    var allDataTypes = Set<HKSampleType>()
 
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "flutter_health", binaryMessenger: registrar.messenger())
@@ -13,23 +15,12 @@ public class SwiftFlutterHealthPlugin: NSObject, FlutterPlugin {
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    /// CHECK IF HEALTHDATA AVAILABLE METHOD CALL
-    if(call.method.elementsEqual("checkIfHealthDataAvailable")){
-        result(HKHealthStore.isHealthDataAvailable())
-    } 
-    ///REQUEST AUTH METHOD CALL
-    else if(call.method.elementsEqual("requestAuthorization")){
-        var heartRateEventTypes = Set<HKSampleType>()
-        if #available(iOS 12.2, *){
-            heartRateEventTypes =  Set([
-                HKSampleType.categoryType(forIdentifier: .highHeartRateEvent)!,
-                HKSampleType.categoryType(forIdentifier: .lowHeartRateEvent)!,
-                HKSampleType.categoryType(forIdentifier: .irregularHeartRhythmEvent)!,
-               ])
-        }
-        if #available(iOS 11.0, *) {
-            let allDataTypes = Set(heartRateEventTypes + healthDataTypes)
+  func checkIfHealthDataAvailable(call: FlutterMethodCall, result: @escaping FlutterResult) {
+      result(HKHealthStore.isHealthDataAvailable())
+  }
+
+  func requestAuthorization(call: FlutterMethodCall, result: @escaping FlutterResult) {
+      if #available(iOS 11.0, *) {
             healthStore.requestAuthorization(toShare: nil, read: allDataTypes) { (success, error) in
                 result(success)
             }
@@ -37,88 +28,108 @@ public class SwiftFlutterHealthPlugin: NSObject, FlutterPlugin {
         else {
             result(false)// Handle the error here.
         }
-    } 
-    /// GET DATA METHOD CALL
-    else if(call.method.elementsEqual("getData")){
-        let arguments = call.arguments as? NSDictionary
-        let index = (arguments?["index"] as? Int) ?? -1
-        let startDate = (arguments?["startDate"] as? NSNumber) ?? 0
-        let endDate = (arguments?["endDate"] as? NSNumber) ?? 0
-        
-        
-        let dateFrom = Date(timeIntervalSince1970: startDate.doubleValue / 1000)
-        let dateTo = Date(timeIntervalSince1970: endDate.doubleValue / 1000)
-        
-        if #available(iOS 11.0, *) {
-            healthDataTypes = [
-                HKSampleType.quantityType(forIdentifier: .bodyFatPercentage)!,
-                HKSampleType.quantityType(forIdentifier: .height)!,
-                HKSampleType.quantityType(forIdentifier: .bodyMassIndex)!,
-                HKSampleType.quantityType(forIdentifier: .waistCircumference)!,
-                HKSampleType.quantityType(forIdentifier: .stepCount)!,
-                HKSampleType.quantityType(forIdentifier: .basalEnergyBurned)!,
-                HKSampleType.quantityType(forIdentifier: .activeEnergyBurned)!,
-                HKSampleType.quantityType(forIdentifier: .heartRate)!,
-                HKSampleType.quantityType(forIdentifier: .bodyTemperature)!,
-                HKSampleType.quantityType(forIdentifier: .bloodPressureSystolic)!,
-                HKSampleType.quantityType(forIdentifier: .bloodPressureDiastolic)!,
-                HKSampleType.quantityType(forIdentifier: .restingHeartRate)!,
-                HKSampleType.quantityType(forIdentifier: .walkingHeartRateAverage)!,
-                HKSampleType.quantityType(forIdentifier: .oxygenSaturation)!,
-                HKSampleType.quantityType(forIdentifier: .bloodGlucose)!,
-                HKSampleType.quantityType(forIdentifier: .electrodermalActivity)!,
-                ]
-            print("INDEX IS " , index)
-            print("COUNT IS " , healthDataTypes.count)
-            if(index >= 0 && index < healthDataTypes.count){
-                let dataType = healthDataTypes[index]
-                print("DATA TYPE IS ", dataType)
-                let predicate = HKQuery.predicateForSamples(withStart: dateFrom, end: dateTo, options: .strictStartDate)
-                print("PREDICATE ", predicate)
-                if (self.healthStore.authorizationStatus(for: dataType) == .sharingAuthorized) {
-                }
+  }
 
-                let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: true)
+  func getData(call: FlutterMethodCall, result: @escaping FlutterResult) {
+    let arguments = call.arguments as? NSDictionary
+    let index = (arguments?["index"] as? Int) ?? -1
+    let startDate = (arguments?["startDate"] as? NSNumber) ?? 0
+    let endDate = (arguments?["endDate"] as? NSNumber) ?? 0
+    let dateFrom = Date(timeIntervalSince1970: startDate.doubleValue / 1000)
+    let dateTo = Date(timeIntervalSince1970: endDate.doubleValue / 1000)
 
-                
-                let query = HKSampleQuery(sampleType: dataType, predicate: predicate, limit: HKObjectQueryNoLimit, sortDescriptors: [sortDescriptor]) {
-                    x, samplesOrNil, error in
-                    
-                    guard let samples = samplesOrNil as? [HKQuantitySample] else {
-                        result(FlutterError(code: "FlutterHealth", message: "Results are null", details: error))
-                        return
-                    }
+    if (index >= 0 && index < healthDataTypes.count) {
+        let dataType = healthDataTypes[index]
+        let predicate = HKQuery.predicateForSamples(withStart: dateFrom, end: dateTo, options: .strictStartDate)
+        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: true)
 
-                    if(samples != nil){
-                    result(samples.map { sample -> NSDictionary in
-                        let unit = self.unitFromDartType(type: index)
-                        return [
-                            "value": sample.quantity.doubleValue(for: unit),
-                            "unit": unit.unitString,
-                            "date_from": Int(sample.startDate.timeIntervalSince1970 * 1000),
-                            "date_to": Int(sample.endDate.timeIntervalSince1970 * 1000),
-                            "data_type_index": index,
-                        ]
-                    })
-                    } else {
-                        print("Either there are no values or the user did not allow getting this value")
-                        result("Either there are no values or the user did not allow getting this value")
-                    }
-                    return
-                }
-                HKHealthStore().execute(query)
-            } else{
-                print("Something wrong with request")
-                result("Unsupported version or data type")
+        let query = HKSampleQuery(sampleType: dataType, predicate: predicate, limit: HKObjectQueryNoLimit, sortDescriptors: [sortDescriptor]) {
+            x, samplesOrNil, error in
+            
+            guard let samples = samplesOrNil as? [HKQuantitySample] else {
+                result(FlutterError(code: "FlutterHealth", message: "Results are null", details: error))
+                return
             }
-        } else {
-            print("Unsupported version 1")
-            result("Unsupported version or data type")
+
+            if(samples != nil){
+            result(samples.map { sample -> NSDictionary in
+                let unit = self.unitFromDartType(type: index)
+                return [
+                    "value": sample.quantity.doubleValue(for: unit),
+                    "unit": unit.unitString,
+                    "date_from": Int(sample.startDate.timeIntervalSince1970 * 1000),
+                    "date_to": Int(sample.endDate.timeIntervalSince1970 * 1000),
+                    "data_type_index": index,
+                ]
+            })
+            } else {
+                print("Either there are no values or the user did not allow getting this value")
+                result("Either there are no values or the user did not allow getting this value")
+            }
+            return
         }
-        print("Unsupported version")
+        HKHealthStore().execute(query)
+    } 
+    else {
+        print("Something wrong with request")
+        result("Unsupported version or data type")
+    }
+    print("Unsupported version")
+  }
+
+  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    // Set up all data types
+    initializeTypes()
+    
+    /// Handle checkIfHealthDataAvailable
+    if(call.method.elementsEqual("checkIfHealthDataAvailable")){
+        checkIfHealthDataAvailable(call: call, result: result)
+    }
+    /// Handle requestAuthorization
+    else if(call.method.elementsEqual("requestAuthorization")){
+        requestAuthorization(call: call, result: result)
+    }
+
+    /// Handle getData
+    else if(call.method.elementsEqual("getData")){
+        getData(call: call, result: result)
     }
     
   }
+    func initializeTypes() {
+        // Set up iOS 11 specific types (ordinary health data types)
+        if #available(iOS 11.0, *) { 
+            healthDataTypes = [
+                    HKSampleType.quantityType(forIdentifier: .bodyFatPercentage)!,
+                    HKSampleType.quantityType(forIdentifier: .height)!,
+                    HKSampleType.quantityType(forIdentifier: .bodyMassIndex)!,
+                    HKSampleType.quantityType(forIdentifier: .waistCircumference)!,
+                    HKSampleType.quantityType(forIdentifier: .stepCount)!,
+                    HKSampleType.quantityType(forIdentifier: .basalEnergyBurned)!,
+                    HKSampleType.quantityType(forIdentifier: .activeEnergyBurned)!,
+                    HKSampleType.quantityType(forIdentifier: .heartRate)!,
+                    HKSampleType.quantityType(forIdentifier: .bodyTemperature)!,
+                    HKSampleType.quantityType(forIdentifier: .bloodPressureSystolic)!,
+                    HKSampleType.quantityType(forIdentifier: .bloodPressureDiastolic)!,
+                    HKSampleType.quantityType(forIdentifier: .restingHeartRate)!,
+                    HKSampleType.quantityType(forIdentifier: .walkingHeartRateAverage)!,
+                    HKSampleType.quantityType(forIdentifier: .oxygenSaturation)!,
+                    HKSampleType.quantityType(forIdentifier: .bloodGlucose)!,
+                    HKSampleType.quantityType(forIdentifier: .electrodermalActivity)!,
+                    ]
+        }
+        // Set up heart rate data types specific to the apple watch, requires iOS 12
+        if #available(iOS 12.2, *){
+            heartRateEventTypes =  Set([
+                HKSampleType.categoryType(forIdentifier: .highHeartRateEvent)!,
+                HKSampleType.categoryType(forIdentifier: .lowHeartRateEvent)!,
+                HKSampleType.categoryType(forIdentifier: .irregularHeartRhythmEvent)!,
+                ])
+        }
+
+        // Concatenate heart events and health data types (both may be empty)
+        allDataTypes = Set(heartRateEventTypes + healthDataTypes)
+    }
     
     public func unitFromDartType(type: Int) -> HKUnit {
         guard let unit: HKUnit = {
