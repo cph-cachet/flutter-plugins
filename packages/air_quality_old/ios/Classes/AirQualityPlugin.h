cf962b829915286a560ef0894f60efda517b56c8
thomasnilsson <s144470@student.dtu.dk>
2020-04-17 10:33:57 +0200

    added example to air quality package

--- packages/air_quality_old/ios/Classes/AirQualityPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface AirQualityPlugin : NSObject<FlutterPlugin>
-@end
--- packages/air_quality_old/ios/Classes/AirQualityPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "AirQualityPlugin.h"
-#import <air_quality/air_quality-Swift.h>
-
-@implementation AirQualityPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftAirQualityPlugin registerWithRegistrar:registrar];
-}
-@end
