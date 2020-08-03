4c949ed4926a1556ba6004c8614c25fe7bcbfebe
thomasnilsson <s144470@student.dtu.dk>
2020-08-03 14:25:12 +0200

    removed step detection and added the home page link to pedometer

--- packages/step_detection/example/android/app/src/main/kotlin/dk/cachet/step_detection_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package dk.cachet.step_detection_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- packages/step_detection/ios/Classes/StepDetectionPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface StepDetectionPlugin : NSObject<FlutterPlugin>
-@end
