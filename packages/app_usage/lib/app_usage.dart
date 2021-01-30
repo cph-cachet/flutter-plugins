e34b0e213a1990f70b191d663de3122f0f3d90a7
thomasnilsson <tnk.videnskab@gmail.com>
2021-01-30 13:20:52 +0100

    Fix a semicolon and log all output to terminal in example app

--- packages/app_usage/lib/app_usage.dart
+++ packages/app_usage/lib/app_usage.dart
@@ -24,7 +24,7 @@ class AppUsageInfo {
   AppUsageInfo(
       String name, double usageInSeconds, this._startDate, this._endDate) {
     List<String> tokens =  name.split('.');
-    _packageName = name
+    _packageName = name;
     _appName = tokens.last;
     _usage = Duration(seconds: usageInSeconds.toInt());
   }
