712e17079d0e5ae02b57a2e443be12180e5c0409
Tilo C <github@legentix.com>
2021-02-27 21:51:05 +0100

    Fix Android example app.

--- packages/health/example/android/app/src/main/kotlin/cachet/plugins/health_example/MainActivity.kt
+++ packages/health/example/android/app/src/main/kotlin/cachet/plugins/health_example/MainActivity.kt
@@ -2,7 +2,7 @@ package cachet.plugins.health_example
 
 import android.os.Bundle
 
-import io.flutter.app.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
 }
