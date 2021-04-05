7ae7ecb6c8021706dfcf894c46c865082f479504
Thomas Nilsson <9467047+thomasnilsson@users.noreply.github.com>
2021-04-05 16:03:26 +0200

    notifications v 1.1.0

--- packages/notifications/example/lib/main.dart
+++ packages/notifications/example/lib/main.dart
@@ -53,7 +53,7 @@ class _MyAppState extends State<MyApp> {
     return new MaterialApp(
       home: new Scaffold(
         appBar: new AppBar(
-          title: const Text('Screen State Example app'),
+          title: const Text('Notifications Example app'),
         ),
         body: new Center(
             child: new ListView.builder(
