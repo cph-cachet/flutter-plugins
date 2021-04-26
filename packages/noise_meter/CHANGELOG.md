f5301104fd38babf0b05e9eccf82a3b925cca183
Mads Christensen <s174238@student.dtu.dk>
2021-04-26 12:04:31 +0200

    [Noise Meter] Null safety migration -- Release 3.0.0

--- packages/noise_meter/CHANGELOG.md
+++ packages/noise_meter/CHANGELOG.md
@@ -1,15 +1,25 @@
+## 3.0.0
+
+- Null safety migration
+- iOS example podfile update
+
 ## 2.0.0
-* Changed API to support audio_streamer version 1.3.0
-* Changed documentation
+
+- Changed API to support audio_streamer version 1.3.0
+- Changed documentation
 
 ## 1.2.0
-* Changed to api of a NoiseReading to include max and mean decibel.
+
+- Changed to api of a NoiseReading to include max and mean decibel.
 
 ## 1.1.5
-* Added a getter for the sample rate field.
+
+- Added a getter for the sample rate field.
 
 ## 1.1.0
-* Moved native source code to the AudioStreamer plugin (https://pub.dev/packages/audio_streamer).
+
+- Moved native source code to the AudioStreamer plugin (https://pub.dev/packages/audio_streamer).
 
 ## 1.0.0
-* Able to stream NoiseReadings on iOS and Android.
+
+- Able to stream NoiseReadings on iOS and Android.
--- packages/noise_meter/lib/noise_meter.dart
+++ packages/noise_meter/lib/noise_meter.dart
@@ -8,7 +8,7 @@ import 'package:flutter/services.dart';
 
 /// A [NoiseReading] holds a decibel value for a particular noise level reading.
 class NoiseReading {
-  double _meanDecibel, _maxDecibel;
+  late double _meanDecibel, _maxDecibel;
 
   NoiseReading(List<double> volumes) {
     /// Sorted volumes such that the last element is max amplitude
@@ -44,14 +44,14 @@ class NoiseReading {
 class NoiseMeter {
   AudioStreamer _streamer = AudioStreamer();
   Function _onError;
-  Stream<NoiseReading> _stream;
+  Stream<NoiseReading>? _stream;
 
   NoiseMeter(this._onError);
 
   /// The rate at which the audio is sampled
   static int get sampleRate => AudioStreamer.sampleRate;
 
-  StreamController<NoiseReading> _controller;
+  late StreamController<NoiseReading> _controller;
 
   Stream<NoiseReading> get noiseStream {
     if (_stream == null) {
@@ -59,7 +59,7 @@ class NoiseMeter {
           onListen: _start, onCancel: _stop);
       _stream = _controller.stream.handleError(_onError);
     }
-    return _stream;
+    return _stream!;
   }
 
   /// Whenever an array of PCM data comes in,
