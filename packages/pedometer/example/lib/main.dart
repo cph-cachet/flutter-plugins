f3f8deb64108133f17a3126f30d92dc24400c82a
Nikos Portolos <nikos.portolos@gmail.com>
2021-05-10 00:25:11 +0300

    Added permissions handling for activity recognition

--- packages/pedometer/example/lib/main.dart
+++ packages/pedometer/example/lib/main.dart
@@ -1,11 +1,11 @@
+import 'package:permission_handler/permission_handler.dart';
+import 'package:device_info/device_info.dart';
+import 'package:pedometer/pedometer.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'dart:io';
 
-import 'package:pedometer/pedometer.dart';
-
-String formatDate(DateTime d) {
-  return d.toString().substring(0, 19);
-}
+String formatDate(DateTime d) => d.toString().substring(0, 19);
 
 void main() {
   runApp(MyApp());
@@ -28,39 +28,51 @@ class _MyAppState extends State<MyApp> {
   }
 
   void onStepCount(StepCount event) {
-    print(event);
-    setState(() {
-      _steps = event.steps.toString();
-    });
+    print('=====> $event');
+    setState(() => _steps = event.steps.toString());
   }
 
   void onPedestrianStatusChanged(PedestrianStatus event) {
-    print(event);
-    setState(() {
-      _status = event.status;
-    });
+    print('=====> $event');
+    setState(() => _status = event.status);
   }
 
   void onPedestrianStatusError(error) {
     print('onPedestrianStatusError: $error');
-    setState(() {
-      _status = 'Pedestrian Status not available';
-    });
+    setState(() => _status = 'Pedestrian Status not available');
     print(_status);
   }
 
   void onStepCountError(error) {
     print('onStepCountError: $error');
-    setState(() {
-      _steps = 'Step Count not available';
-    });
+    setState(() => _steps = 'Step Count not available');
   }
 
-  void initPlatformState() {
+  Future<Permission> getMotionPermission() async {
+    if (Platform.isAndroid && await getAndroidSdk() >= 29)
+      return Permission.activityRecognition;
+    else
+      return Permission.sensors;
+  }
+
+  Future<int> getAndroidSdk() async {
+    if (Platform.isAndroid) {
+      var androidInfo = await DeviceInfoPlugin().androidInfo;
+      var sdkInt = androidInfo.version.sdkInt;
+      return sdkInt;
+    }
+    return 0;
+  }
+
+  Future<void> initPlatformState() async {
+    Permission motionPermission = await getMotionPermission();
+    if (!await motionPermission.isGranted) {
+      var newStatus = await motionPermission.request();
+      if (!newStatus.isGranted) return Future.error('Permission not granted');
+    }
+
     _pedestrianStatusStream = Pedometer.pedestrianStatusStream;
-    _pedestrianStatusStream
-        .listen(onPedestrianStatusChanged)
-        .onError(onPedestrianStatusError);
+    _pedestrianStatusStream.listen(onPedestrianStatusChanged).onError(onPedestrianStatusError);
 
     _stepCountStream = Pedometer.stepCountStream;
     _stepCountStream.listen(onStepCount).onError(onStepCountError);
@@ -72,45 +84,24 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Pedometer example app'),
-        ),
+        appBar: AppBar(title: const Text('Pedometer example app')),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text(
-                'Steps taken:',
-                style: TextStyle(fontSize: 30),
-              ),
-              Text(
-                _steps,
-                style: TextStyle(fontSize: 60),
-              ),
-              Divider(
-                height: 100,
-                thickness: 0,
-                color: Colors.white,
-              ),
-              Text(
-                'Pedestrian status:',
-                style: TextStyle(fontSize: 30),
-              ),
+              Text('Steps taken:', style: TextStyle(fontSize: 30)),
+              Text(_steps, style: TextStyle(fontSize: 60)),
+              Divider(height: 100, thickness: 0, color: Colors.white),
+              Text('Pedestrian status:', style: TextStyle(fontSize: 30)),
               Icon(
-                _status == 'walking'
-                    ? Icons.directions_walk
-                    : _status == 'stopped'
-                        ? Icons.accessibility_new
-                        : Icons.error,
-                size: 100,
-              ),
+                  _status == 'walking'
+                      ? Icons.directions_walk
+                      : _status == 'stopped'
+                          ? Icons.accessibility_new
+                          : Icons.error,
+                  size: 100),
               Center(
-                child: Text(
-                  _status,
-                  style: _status == 'walking' || _status == 'stopped'
-                      ? TextStyle(fontSize: 30)
-                      : TextStyle(fontSize: 20, color: Colors.red),
-                ),
+                child: Text(_status, style: _status == 'walking' || _status == 'stopped' ? TextStyle(fontSize: 30) : TextStyle(fontSize: 20, color: Colors.red)),
               )
             ],
           ),
