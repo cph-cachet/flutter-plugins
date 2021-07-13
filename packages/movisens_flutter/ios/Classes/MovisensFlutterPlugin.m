314fdb4fd342fae1a6f57e93962665e80dc36cde
bardram <jakba@dtu.dk>
2021-07-13 19:06:00 +0200

    movisens_flutter 0.2.0 published

--- packages/movisens_flutter/ios/Classes/MovisensFlutterPlugin.m
+++ packages/movisens_flutter/ios/Classes/MovisensFlutterPlugin.m
@@ -1,20 +1,15 @@
 #import "MovisensFlutterPlugin.h"
+#if __has_include(<movisens_flutter/movisens_flutter-Swift.h>)
+#import <movisens_flutter/movisens_flutter-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "movisens_flutter-Swift.h"
+#endif
 
 @implementation MovisensFlutterPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"movisens_flutter"
-            binaryMessenger:[registrar messenger]];
-  MovisensFlutterPlugin* instance = [[MovisensFlutterPlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+  [SwiftMovisensFlutterPlugin registerWithRegistrar:registrar];
 }
-
-- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
-}
-
 @end
