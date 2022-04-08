499f20009b4b0ea4897c07f206192d3070db8ab5
Lars Refsgaard <1552887+lkbr1808@users.noreply.github.com>
2022-04-08 13:37:52 +0200

    fleshed out the beginning of the android e4 manager

---
+++ packages/empatica_e4link/android/src/main/java/dk/cachet/empatica_e4link/EmpaticaE4DeviceManagerMethodCallHandler.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2019 Copenhagen Center for Health Technology (CACHET) at the
+ * Technical University of Denmark (DTU).
+ * Use of this source code is governed by a MIT-style license that can be
+ * found in the LICENSE file.
+ */
+package dk.cachet.empatica_e4link;
+
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel.*;
+
+import java.util.Enumeration;
+
+import android.content.Context;
+import io.esense.esenselib.*;
+
+public class EmpaticaE4DeviceManagerMethodCallHandler implements MethodCallHandler {
+
+    public static final int TIMEOUT = 5 * 1000;
+
+    private boolean connected = false;
+    private Context context;
+    private EmpaticaE4ConnectionEventStreamHandler empaticaE4ConnectionEventStreamHandler;
+
+    private int samplingRate = 10; // default 10 Hz.
+    EmpaDeviceManager manager;
+
+    public EmpaticaE4DeviceManagerMethodCallHandler(
+            Context context,
+            EmpaticaE4ConnectionEventStreamHandler empaticaE4ConnectionEventStreamHandler) {
+        this.context = context;
+        this.empaticaE4ConnectionEventStreamHandler = empaticaE4ConnectionEventStreamHandler;
+        manager = new EmpaDeviceManager(getApplicationContext(), this, this);
+    }
+
+    /**
+     * The current sampling rate as specified in the [setSamplingRate] method
+     *
+     * @return the sampling rate in Hz.
+     */
+    public int getSamplingRate() {
+        return samplingRate;
+    }
+
+    @Override
+    public void onMethodCall(MethodCall call, Result rawResult) {
+        Result result = new MainThreadResult(rawResult);
+        boolean success;
+
+        switch (call.method) {
+            case "authenticateWithAPIKey":
+                final String key = call.argument("key");
+                success = manager.authenticateWithAPIKey(key);
+                result.success(success);
+                break;
+            case "connectDevice":
+                android.bluetooth.BluetoothDevice device = call.argument("device");
+                success = manager.connectDevice(device);
+                result.success(success);
+                break;
+            case "disconnect":
+                success = manager.disconnect();
+                result.success(success);
+                break;
+            case "getActiveDevice":
+                success = manager.getActiveDevice();
+                result.success(success);
+                break;
+            case "startScanning":
+                success = manager.startScanning();
+                result.success(success);
+                break;
+            case "stopScanning":
+                success = manager.stopScanning();
+                result.success(success);
+                break;
+            default:
+                result.notImplemented();
+        }
+    }
+}
--- packages/empatica_e4link/lib/empatica_e4link.dart
+++ packages/empatica_e4link/lib/empatica_e4link.dart
@@ -1,13 +1,23 @@
+library empaticae4;
 
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 
 class EmpaticaE4link {
+
+
   static const MethodChannel _channel = MethodChannel('empatica_e4link');
 
   static Future<String?> get platformVersion async {
     final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  Future<bool> authenticateWithAPIKey() async {
+    final bool result = await _channel.invokeMethod('authenticateWithAPIKey', <String, dynamic>{
+      'apiKey': '4daa8ba5bc4b419c81b597d6e592289b',
+    });
+    return result;
   }
 }
