f81c701166c5be90735b4a4677d6fc4d13a68db9
Lars Refsgaard <1552887+lkbr1808@users.noreply.github.com>
2022-04-11 12:29:39 +0200

    more safely implemented methods where methods are named and not just strings

--- packages/empatica_e4link/android/src/main/java/dk/cachet/empatica_e4link/EmpaticaE4linkDeviceManagerMethodCallHandler.java
+++ packages/empatica_e4link/android/src/main/java/dk/cachet/empatica_e4link/EmpaticaE4linkDeviceManagerMethodCallHandler.java
@@ -15,17 +15,18 @@ import java.util.Map;
 import android.content.Context;
 import com.empatica.empalink.*;
 
-public class EmpaticaE4DeviceManagerMethodCallHandler extends AppCompatActivity implements EmpaDataDelegate, EmpaStatusDelegate {
+public class EmpaticaE4DeviceManagerMethodCallHandler extends AppCompatActivity
+        implements EmpaDataDelegate, EmpaStatusDelegate {
     final MethodChannel channel;
-    final private EmpaDeviceManager _manager;
+    final private EmpaDeviceManager empaticaManager;
     Map<String, EmpaticaDevice> discoveredDevices = new HashMap<>();
-
 
     private int samplingRate = 10; // default 10 Hz.
 
-    public EmpaticaE4DeviceManagerMethodCallHandler(
+    public EmpaticaE4DeviceManagerMethodCallHandler(MethodChannel channel,
             Context context) {
-        manager = new EmpaDeviceManager(getApplicationContext(), this, this);
+        this.channel = channel;
+        this._manager = new EmpaDeviceManager(getApplicationContext(), this, this);
     }
 
     /**
@@ -35,6 +36,36 @@ public class EmpaticaE4DeviceManagerMethodCallHandler extends AppCompatActivity 
      */
     public int getSamplingRate() {
         return samplingRate;
+    }
+
+    public Result authenticateWithAPIKey(String apiKey) {
+        return result.success(
+                this.empaticaManager.authenticateWithAPIKey(apiKey));
+    }
+
+    public Result connectDevice(android.bluetooth.BluetoothDevice device) {
+        return result.success(
+                this.empaticaManager.connectDevice(device)); // rethink. maybe use discoveredDevices
+    }
+
+    public Result disconnect() {
+        return result.success(
+                this.empaticaManager.disconnect());
+    }
+
+    public Result getActiveDevice() {
+        return result.success(
+                this.empaticaManager.getActiveDevice());
+    }
+
+    public Result startScanning() {
+        return result.success(
+                this.empaticaManager.startScanning()); // fill discoveredDevices when scanning
+    }
+
+    public Result stopScanning() {
+        return result.success(
+                this.empaticaManager.stopScanning());
     }
 
     @Override
