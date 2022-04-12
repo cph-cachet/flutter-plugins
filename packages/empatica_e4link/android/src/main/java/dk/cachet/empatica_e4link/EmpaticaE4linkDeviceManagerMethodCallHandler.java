77b71dca0d3ab59cd4d17f6d2b5f3870c1938b95
Lars Refsgaard <1552887+lkbr1808@users.noreply.github.com>
2022-04-12 14:37:00 +0200

    I've managed to build the aar now. This makes intellisense show up
    This intellisense means we can't code split, as it does not work as such. Will have to think of a good solution to this.

--- packages/empatica_e4link/android/src/main/java/dk/cachet/empatica_e4link/EmpaticaE4linkDeviceManagerMethodCallHandler.java
+++ packages/empatica_e4link/android/src/main/java/dk/cachet/empatica_e4link/EmpaticaE4linkDeviceManagerMethodCallHandler.java
@@ -6,65 +6,23 @@
  */
 package dk.cachet.empatica_e4link;
 
-import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.MethodChannel.*;
+import android.content.Context;
 
-import java.util.Enumeration;
+import com.empatica.empalink.EmpaDeviceManager;
+import com.empatica.empalink.EmpaticaDevice;
+import com.empatica.empalink.config.EmpaSensorType;
+import com.empatica.empalink.config.EmpaStatus;
+import com.empatica.empalink.delegate.EmpaDataDelegate;
+import com.empatica.empalink.delegate.EmpaStatusDelegate;
+
+import java.util.HashMap;
 import java.util.Map;
 
-import android.content.Context;
-import com.empatica.empalink.*;
+import io.flutter.plugin.common.MethodChannel.*;
 
-public class EmpaticaE4DeviceManagerMethodCallHandler extends AppCompatActivity
-        implements EmpaDataDelegate, EmpaStatusDelegate {
+public class EmpaticaE4DeviceManagerMethodCallHandler extends EmpaDeviceManager implements EmpaDataDelegate, EmpaStatusDelegate {
     final MethodChannel channel;
     final private EmpaDeviceManager empaticaManager;
     Map<String, EmpaticaDevice> discoveredDevices = new HashMap<>();
 
-    private int samplingRate = 10; // default 10 Hz.
-
-    public EmpaticaE4DeviceManagerMethodCallHandler(MethodChannel channel,
-            Context context) {
-        this.channel = channel;
-        this._manager = new EmpaDeviceManager(getApplicationContext(), this, this);
-    }
-
-    /**
-     * The current sampling rate as specified in the [setSamplingRate] method
-     *
-     * @return the sampling rate in Hz.
-     */
-    public int getSamplingRate() {
-        return samplingRate;
-    }
-
-    public Result authenticateWithAPIKey(String apiKey) {
-        return result.success(
-                this.empaticaManager.authenticateWithAPIKey(apiKey));
-    }
-
-    public Result connectDevice(android.bluetooth.BluetoothDevice device) {
-        return result.success(
-                this.empaticaManager.connectDevice(device)); // rethink. maybe use discoveredDevices
-    }
-
-    public Result disconnect() {
-        return result.success(
-                this.empaticaManager.disconnect());
-    }
-
-    public Result getActiveDevice() {
-        return result.success(
-                this.empaticaManager.getActiveDevice());
-    }
-
-    public Result startScanning() {
-        return result.success(
-                this.empaticaManager.startScanning()); // fill discoveredDevices when scanning
-    }
-
-    public Result stopScanning() {
-        return result.success(
-                this.empaticaManager.stopScanning());
-    }
 }
