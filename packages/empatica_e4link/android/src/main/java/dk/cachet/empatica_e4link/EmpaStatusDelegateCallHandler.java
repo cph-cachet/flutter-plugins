64ed3421cd9b5ca8a8ef7598f8731284757d5af5
Lars Refsgaard <1552887+lkbr1808@users.noreply.github.com>
2022-04-19 14:57:17 +0200

    first implementations of callbacks

--- packages/empatica_e4link/android/src/main/java/dk/cachet/empatica_e4link/EmpaStatusDelegateCallHandler.java
+++ packages/empatica_e4link/android/src/main/java/dk/cachet/empatica_e4link/EmpaStatusDelegateCallHandler.java
@@ -1,14 +1,20 @@
 package dk.cachet.empatica_e4link;
 
 import com.empatica.empalink.EmpaticaDevice;
+import com.empatica.empalink.config.EmpaSensorStatus;
 import com.empatica.empalink.config.EmpaSensorType;
 import com.empatica.empalink.config.EmpaStatus;
 import com.empatica.empalink.delegate.EmpaStatusDelegate;
 
+import io.flutter.plugin.common.MethodChannel.*;
+
 public class EmpaStatusDelegateCallHandler implements EmpaStatusDelegate {
+    final MethodChannel channel;
+
+
     @Override
     public void didUpdateStatus(EmpaStatus status) {
-        channek
+        channel.invokeMethod("didUpdateStatus", status);
     }
 
     @Override
@@ -17,13 +23,14 @@ public class EmpaStatusDelegateCallHandler implements EmpaStatusDelegate {
     }
 
     @Override
-    public void didUpdateSensorStatus(int status, EmpaSensorType type) {
-
+    public void didUpdateSensorStatus(@EmpaSensorStatus int status, EmpaSensorType type) {
+        channel.invokeMethod("didUpdateSensorStatus", status, type);
     }
 
     @Override
     public void didDiscoverDevice(EmpaticaDevice device, String deviceLabel, int rssi, boolean allowed) {
-
+        if (!allowed) return;
+        channel.invokeMethod("didDiscoverDevice", device, deviceLabel, rssi);
     }
 
     @Override
@@ -43,6 +50,6 @@ public class EmpaStatusDelegateCallHandler implements EmpaStatusDelegate {
 
     @Override
     public void didUpdateOnWristStatus(int status) {
-
+        channel.invokeMethod("didUpdateOnWristStatus", status);
     }
 }
