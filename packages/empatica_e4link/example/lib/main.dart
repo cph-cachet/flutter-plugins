f9bb2ccf103f3300550c6e0b0d3e641b2fb24f57
Lars Refsgaard <1552887+LarsRefsgaard@users.noreply.github.com>
2022-04-25 17:21:18 +0200

    Fixes so the example app will work

--- packages/empatica_e4link/example/lib/main.dart
+++ packages/empatica_e4link/example/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:flutter/services.dart';
-
+import 'package:empatica_e4link/empatica_e4.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,34 +16,17 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
+  EmpaDeviceManager deviceManager = EmpaDeviceManager();
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
+    _connectToAPI();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion = EmpaDeviceManager;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
+  Future<void> _connectToAPI() async {
+    await deviceManager
+        .authenticateWithAPIKey("apiKeyGoesHere");
   }
 
   @override
@@ -53,8 +36,8 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: const Center(
+          child: Text('Running on: nothing\n'),
         ),
       ),
     );
