8b2aef1d9cd5c64169d2851f98edd848bba6d053
Lars Refsgaard <1552887+lkbr1808@users.noreply.github.com>
2022-05-30 10:47:49 +0200

    log battery levels when available

--- packages/empatica_e4link/lib/empatica.dart
+++ packages/empatica_e4link/lib/empatica.dart
@@ -25,22 +25,37 @@ class EmpaticaPlugin {
   EmpaStatus status = EmpaStatus.initial;
   // ------------    METHOD HANDLERS --------------------
 
+
+  /// Initiates a connection to the Empatica backend using an API key given by Empatica.
+  ///
+  /// The [EmpaStatus.ready] status will be thrown on the [statusEventSink] when
+  /// the authentication is accepted.
   Future<void> authenticateWithAPIKey(String key) async {
     await _methodChannel.invokeMethod('authenticateWithAPIKey', {'key': key});
   }
 
+  /// Starts scanning for Empatica devices once the [EmpaStatus.ready] is thrown.
+  ///
+  /// Once a device is found, the [statusEventSink] will throw a
+  /// [DiscoverDevice] event. This event contains the device's serial number,
+  /// along with a label with the MAC address and the RSSI connection strength.
   Future<void> startScanning() async {
     await _methodChannel.invokeMethod('startScanning');
   }
 
+  /// Connect with Empatica connect user. Unknown how this works exactly.
   Future<void> authenticateWithConnectUser() async {
     await _methodChannel.invokeMethod('authenticateWithConnectUser');
   }
 
+  /// Stops the scanning for devices started by [startScanning]. Suitably call this whenever one has connected
+  /// to the device using [connectDevice].
   Future<void> stopScanning() async {
     await _methodChannel.invokeMethod('stopScanning');
   }
 
+  /// Connects to the device with the serial number. Suitably call this whenever
+  /// one has found a device from [startScanning]
   Future<void> connectDevice(String serialNumber) async {
     await _methodChannel
         .invokeMethod('connectDevice', {'serialNumber': serialNumber});
