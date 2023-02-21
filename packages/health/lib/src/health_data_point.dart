f9a3bd3a65b51473c3e823f76e1bd95af8197704
António Valente <antonio@snaq.io>
2023-02-21 18:53:01 +0000

    add metadata to health data point

--- packages/health/lib/src/health_data_point.dart
+++ packages/health/lib/src/health_data_point.dart
@@ -12,17 +12,20 @@ class HealthDataPoint {
   String _deviceId;
   String _sourceId;
   String _sourceName;
+  Map<String, dynamic>? _metadata;
 
   HealthDataPoint(
-      this._value,
-      this._type,
-      this._unit,
-      this._dateFrom,
-      this._dateTo,
-      this._platform,
-      this._deviceId,
-      this._sourceId,
-      this._sourceName) {
+    this._value,
+    this._type,
+    this._unit,
+    this._dateFrom,
+    this._dateTo,
+    this._platform,
+    this._deviceId,
+    this._sourceId,
+    this._sourceName,
+    this._metadata,
+  ) {
     // set the value to minutes rather than the category
     // returned by the native API
     if (type == HealthDataType.MINDFULNESS ||
@@ -55,19 +58,20 @@ class HealthDataPoint {
     }
 
     return HealthDataPoint(
-        healthValue,
-        HealthDataType.values
-            .firstWhere((element) => element.name == json['data_type']),
-        HealthDataUnit.values
-            .firstWhere((element) => element.name == json['unit']),
-        DateTime.parse(json['date_from']),
-        DateTime.parse(json['date_to']),
-        PlatformTypeJsonValue.keys.toList()[PlatformTypeJsonValue.values
-            .toList()
-            .indexOf(json['platform_type'])],
-        json['device_id'],
-        json['source_id'],
-        json['source_name']);
+      healthValue,
+      HealthDataType.values
+          .firstWhere((element) => element.name == json['data_type']),
+      HealthDataUnit.values
+          .firstWhere((element) => element.name == json['unit']),
+      DateTime.parse(json['date_from']),
+      DateTime.parse(json['date_to']),
+      PlatformTypeJsonValue.keys.toList()[
+          PlatformTypeJsonValue.values.toList().indexOf(json['platform_type'])],
+      json['device_id'],
+      json['source_id'],
+      json['source_name'],
+      json['metadata'],
+    );
   }
 
   /// Converts the [HealthDataPoint] to a json object
@@ -80,7 +84,8 @@ class HealthDataPoint {
         'platform_type': PlatformTypeJsonValue[platform],
         'device_id': deviceId,
         'source_id': sourceId,
-        'source_name': sourceName
+        'source_name': sourceName,
+        'metadata': metadata,
       };
 
   @override
@@ -93,7 +98,8 @@ class HealthDataPoint {
     platform: $platform,
     deviceId: $deviceId,
     sourceId: $sourceId,
-    sourceName: $sourceName""";
+    sourceName: $sourceName
+    metadata: $metadata""";
 
   // / The quantity value of the data point
   HealthValue get value => _value;
@@ -128,6 +134,9 @@ class HealthDataPoint {
   /// The name of the source from which the data point was fetched.
   String get sourceName => _sourceName;
 
+  /// Metadata contains extra information describing the data point.
+  Map<String, dynamic>? get metadata => _metadata;
+
   @override
   bool operator ==(Object o) {
     return o is HealthDataPoint &&
@@ -139,10 +148,21 @@ class HealthDataPoint {
         this.platform == o.platform &&
         this.deviceId == o.deviceId &&
         this.sourceId == o.sourceId &&
-        this.sourceName == o.sourceName;
+        this.sourceName == o.sourceName &&
+        this.metadata == o.metadata;
   }
 
   @override
-  int get hashCode => Object.hash(value, unit, dateFrom, dateTo, type, platform,
-      deviceId, sourceId, sourceName);
+  int get hashCode => Object.hash(
+        value,
+        unit,
+        dateFrom,
+        dateTo,
+        type,
+        platform,
+        deviceId,
+        sourceId,
+        sourceName,
+        metadata,
+      );
 }
