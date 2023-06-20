c67ce6db3f830527564b1e7d10a94376f65ccc2d
bardram <jakba@dtu.dk>
2023-06-20 18:38:18 +0200

    screen_state version 3.0.0. released

--- packages/screen_state/lib/screen_state.dart
+++ packages/screen_state/lib/screen_state.dart
@@ -2,26 +2,32 @@ import 'dart:async';
 import 'dart:io' show Platform;
 import 'package:flutter/services.dart';
 
-/// Enumeration of Screen State events coming from android.
+/// The type of screen state events coming from Android.
 enum ScreenStateEvent { SCREEN_UNLOCKED, SCREEN_ON, SCREEN_OFF }
 
-/// Custom Exception for the plugin,
-/// thrown whenever the plugin is used on platforms other than Android
+/// Custom Exception for the `screen_state` plugin, used whenever the plugin
+/// is used on platforms other than Android
 class ScreenStateException implements Exception {
   String _cause;
 
   ScreenStateException(this._cause);
 
   @override
-  String toString() {
-    return _cause;
-  }
+  String toString() => '$runtimeType - $_cause';
 }
 
 /// Screen representation as object which holds the stream for [ScreenStateEvent]s.
 class Screen {
+  static Screen? _singleton;
   EventChannel _eventChannel = const EventChannel('screenStateEvents');
   Stream<ScreenStateEvent>? _screenStateStream;
+
+  /// Constructs a singleton instance of [Screen].
+  ///
+  /// [Screen] is designed to work as a singleton.
+  factory Screen() => _singleton ??= Screen._();
+
+  Screen._();
 
   /// Stream of [ScreenStateEvent]s.
   /// Each event is streamed as it occurs on the phone.
@@ -35,13 +41,11 @@ class Screen {
       }
       return _screenStateStream;
     }
-    throw ScreenStateException(
-        'Screen State API exclusively available on Android!');
+    throw ScreenStateException('Screen State API only available on Android');
   }
 
   ScreenStateEvent _parseScreenStateEvent(String event) {
     switch (event) {
-      /** Android **/
       case 'android.intent.action.SCREEN_OFF':
         return ScreenStateEvent.SCREEN_OFF;
       case 'android.intent.action.SCREEN_ON':
