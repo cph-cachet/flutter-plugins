7451738b07d56fe871a8e04834a6cbf885fe288f
levihoang <levihoang.official@outlook.com>
2024-05-30 22:13:18 +0700

    fix: null getCharSequence

--- packages/notifications/android/src/main/java/dk/cachet/notifications/NotificationListener.java
+++ packages/notifications/android/src/main/java/dk/cachet/notifications/NotificationListener.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import androidx.annotation.RequiresApi;
+import android.util.Log;
 
 /**
  * Notification listening service. Intercepts notifications if permission is given to do so.
@@ -16,30 +17,42 @@ import androidx.annotation.RequiresApi;
 @RequiresApi(api = VERSION_CODES.JELLY_BEAN_MR2)
 public class NotificationListener extends NotificationListenerService {
 
-  public static String NOTIFICATION_INTENT = "notification_event";
-  public static String NOTIFICATION_PACKAGE_NAME = "notification_package_name";
-  public static String NOTIFICATION_MESSAGE = "notification_message";
-  public static String NOTIFICATION_TITLE = "notification_title";
+    public static String NOTIFICATION_INTENT = "notification_event";
+    public static String NOTIFICATION_PACKAGE_NAME = "notification_package_name";
+    public static String NOTIFICATION_MESSAGE = "notification_message";
+    public static String NOTIFICATION_TITLE = "notification_title";
 
-  @RequiresApi(api = VERSION_CODES.KITKAT)
-  @Override
-  public void onNotificationPosted(StatusBarNotification notification) {
-    // Retrieve package name to set as title.
-    String packageName = notification.getPackageName();
-    // Retrieve extra object from notification to extract payload.
-    Bundle extras = notification.getNotification().extras;
+    private static final String TAG = "NotificationListener";
 
-    // Pass data from one activity to another.
-    Intent intent = new Intent(NOTIFICATION_INTENT);
-    intent.putExtra(NOTIFICATION_PACKAGE_NAME, packageName);
+    @RequiresApi(api = VERSION_CODES.KITKAT)
+    @Override
+    public void onNotificationPosted(StatusBarNotification notification) {
+        try {
+            // Retrieve package name to set as title.
+            String packageName = notification.getPackageName();
+            // Retrieve extra object from notification to extract payload.
+            Bundle extras = notification.getNotification().extras;
 
-    if (extras != null) {
-      CharSequence title = extras.getCharSequence(Notification.EXTRA_TITLE);
-      CharSequence text = extras.getCharSequence(Notification.EXTRA_TEXT);
+            // Pass data from one activity to another.
+            Intent intent = new Intent(NOTIFICATION_INTENT);
+            intent.putExtra(NOTIFICATION_PACKAGE_NAME, packageName);
 
-      intent.putExtra(NOTIFICATION_TITLE, title.toString());
-      intent.putExtra(NOTIFICATION_MESSAGE, text.toString());
+            if (extras != null) {
+                CharSequence title = null;
+                CharSequence text = null;
+                if (Notification.EXTRA_TITLE != null)
+                    title = extras.getCharSequence(Notification.EXTRA_TITLE);
+                if (Notification.EXTRA_TEXT != null)
+                    text = extras.getCharSequence(Notification.EXTRA_TEXT);
+                String titleStr = title != null ? title.toString() : "";
+                String textStr = text != null ? text.toString() : "";
+                intent.putExtra(NOTIFICATION_TITLE, titleStr);
+                intent.putExtra(NOTIFICATION_MESSAGE, textStr);
+            }
+            sendBroadcast(intent);
+        } catch (Exception error) {
+            Log.getStackTraceString(error);
+//      Log.e(TAG, error);
+        }
     }
-    sendBroadcast(intent);
-  }
 }
