14bdcdebece33d4664afa887da325ad10877e34f
Panagiotis Giannoutsos <36935711+Panosfunk@users.noreply.github.com>
2025-04-02 16:09:53 +0200

    upgrading example app for air_quality

--- packages/air_quality/lib/air_quality.dart
+++ packages/air_quality/lib/air_quality.dart
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Copenhagen Center for Health Technology (CACHET) at the
+ * Copyright 2025 Copenhagen Research Platform (CARP) at the
  * Technical University of Denmark (DTU).
  * Use of this source code is governed by a MIT-style license that can be
  * found in the LICENSE file.
