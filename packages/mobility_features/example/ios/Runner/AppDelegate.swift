f3f9ab7552e3f904985b3cf35d5c060e5d9e9848
Panagiotis Giannoutsos <36935711+Panosfunk@users.noreply.github.com>
2025-04-15 13:15:04 +0300

    mobility features example app ios upgrade

--- packages/mobility_features/example/ios/Runner/AppDelegate.swift
+++ packages/mobility_features/example/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ func registerPlugins(registry: FlutterPluginRegistry) {
   }
 }
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
